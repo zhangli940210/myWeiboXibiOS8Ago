3daed40018d6393bfdbea565174afb435953a361
gj <gj@itcast.cn>
2016-02-23 22:56:46 +0800

    初始化项目

---
+++ 15-自定义不等高的cell-xib(iOS8之前)/15-自定义不等高的cell-xib(iOS8之前)/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  15-自定义不等高的cell-xib(iOS8之前)
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
