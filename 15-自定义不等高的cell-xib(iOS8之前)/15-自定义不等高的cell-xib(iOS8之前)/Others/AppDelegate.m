3ee3c7ef3de2348eca1a72abab236cea1c86d27e
gj <gj@itcast.cn>
2016-02-23 23:10:03 +0800

    完善

---
+++ 15-自定义不等高的cell-xib(iOS8之前)/15-自定义不等高的cell-xib(iOS8之前)/Others/AppDelegate.m
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.m
+//  15-自定义不等高的cell-xib(iOS8之前)
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+@end
